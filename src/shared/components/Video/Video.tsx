0bf0ba76d682a6835185c819f5bd7a6562756f66
Joe Kearney <joe.kearney@mailonline.co.uk>
2021-03-25 16:14:34 +0000

    chore: 🚜 watch types on start

--- src/shared/components/Video/Video.tsx
+++ src/shared/components/Video/Video.tsx
@@ -11,11 +11,13 @@ interface Args {
 
 const Video: React.FC<Args> = (props: Args) => (
   <div className={ styles.videoContainer }>
-    <h3>{props.title}</h3>
+    <div className={styles.title}>
+        <div>{props.title}</div>
+    </div>
     {/* eslint-disable-next-line jsx-a11y/media-has-caption */ }
     <video controls>
       <source
-        src="https://video.dailymail.co.uk/video/mol/2019/04/10/5992941007543840784/960x540_MP4_5992941007543840784.mp4"/>
+        src={props.url}/>
     </video>
   </div>
 );
--- webpack/devServer.ts
+++ webpack/devServer.ts
@@ -4,10 +4,15 @@ import { webpackBundlesServer } from "@mol-fe/mol-fe-webpack-bundles/dist/server
 import { DevServerPayload } from "@mol-fe/mol-fe-webpack-bundles/dist/server/types";
 import { name as pkgName } from '../package.json';
 
+const xpModuleValues = {
+  title: 'Aussies on the Med',
+  url: 'https://video.dailymail.co.uk/video/mol/2019/04/10/5992941007543840784/960x540_MP4_5992941007543840784.mp4'
+};
+
 const xpModuleConfig: DevServerPayload = {
   name: pkgName,
   xpModules: {
-    getValues: () => ({}),
+    getValues: () => xpModuleValues,
     packageDir: path.resolve(__dirname, '..')
   }
 };
